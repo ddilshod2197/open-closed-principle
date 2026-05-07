96a901628ec5b98f6355d96b72d1319260a2dc34
Dilshod <ddilshod2197@gmail.com>
2026-05-07 23:29:45 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,15 @@
+class SingleResponsibilityPrinciple:
+    def __init__(self):
+        pass
+
+    def ishlash(self):
+        return True
+```
+
+```python
+def main():
+    srp = SingleResponsibilityPrinciple()
+    print(srp.ishlash())
+
+if __name__ == "__main__":
+    main()
